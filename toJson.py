013ec7ab8af526ee4a230fd2c9013cc495bae3b7
Pun <80048553+thammanant@users.noreply.github.com>
2023-03-16 14:35:16 +0700

    1. encryptDecrypt.py - 1st version with encrypt and decrypt function, but need to pull the key from database 2. toJson.py - 1st version have addData function and setData function

--- toJson.py
+++ toJson.py
@@ -1,3 +1,24 @@
 import json
 import os
 import sys
+
+
+class toJson:
+    def __init__(self):
+        self.data = {}
+
+    def setData(self, data):
+        self.data = data
+
+    def addData(self):
+        with open('test.json', 'w', encoding='utf-8') as f:
+            json.dump(self.data, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    datas = {'ID': {'Book': {'Book1': 0, 'Book2': 1, 'Book3': 1}, 'E-Book': {'eBook1': 0, 'eBook2': 1, 'eBook3': 1}
+                    }}
+
+    update = toJson()
+    update.setData(datas)
+    update.addData()

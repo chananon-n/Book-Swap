5abb553662051fde37c6d0810d48631db1dda151
Puttiphat U <90830272+Puttiphat-M@users.noreply.github.com>
2023-03-18 01:27:50 +0700

    create Sign-in UI page

---
+++ Sign_in.py
@@ -0,0 +1,88 @@
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+                           QFont, QFontDatabase, QGradient, QIcon,
+                           QImage, QKeySequence, QLinearGradient, QPainter,
+                           QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QPushButton,
+                               QSizePolicy, QSpacerItem, QVBoxLayout, QWidget, QLineEdit)
+from PySide6.QtCore import Qt, QEvent
+
+
+class Sign_in(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        label = QLabel()
+        pixmap = QPixmap(u"Book_Logo.png")
+        label.setPixmap(pixmap)
+        blank = QLabel("")
+
+        h_layout1 = QHBoxLayout()
+        h_layout1.addWidget(blank)
+        h_layout1.addWidget(label)
+        h_layout1.addWidget(blank)
+
+        label1 = QLabel("ID")
+        label1.setFont(QFont("Vesper Libre", 20))
+        label1.setStyleSheet('''
+        QLabel {
+        color: rgb(132, 113, 77);
+        }
+        ''')
+
+        self.enter1 = QLineEdit("Enter the ID")
+        self.enter1.setFont(QFont("Vesper Libre", 20))
+        self.enter1.setStyleSheet('''
+        QLineEdit {
+        border: 3px solid rgb(132, 113, 77);
+        color: rgb(148, 132, 99);
+        }
+        ''')
+
+
+        label2 = QLabel("* 8 digits")
+        label2.setFont(QFont("Vesper Libre", 15))
+        label2.setStyleSheet('''
+        QLabel {
+        color: rgb(255, 0, 0);
+        }
+        ''')
+
+        h_layout2 = QHBoxLayout()
+        h_layout2.addWidget(label1)
+        h_layout2.addSpacing(10)
+        h_layout2.addWidget(self.enter1)
+        h_layout2.addWidget(label2)
+
+        button1 = QPushButton("Sign in")
+        button1.setFont(QFont("Vesper Libre", 25))
+        button1.setStyleSheet('''
+                QPushButton {
+                border: 3px solid rgb(132, 113, 77);
+                color: rgb(249, 246, 236);
+                background-color: rgb(182, 170, 145);
+                }
+                ''')
+
+        h_layout3 = QHBoxLayout()
+        h_layout3.addSpacing(200)
+        h_layout3.addWidget(button1)
+        h_layout3.addSpacing(200)
+
+        v_layout = QVBoxLayout()
+        v_layout.addSpacing(30)
+        v_layout.addLayout(h_layout1)
+        v_layout.addLayout(h_layout2)
+        v_layout.addLayout(h_layout3)
+        v_layout.addSpacing(50)
+
+        myColor = '#920310'
+        self.setLayout(v_layout)
+        self.setStyleSheet("background-color: #F9F6EC;")
+        self.setWindowTitle("Sign in")
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication()
+    ui = Sign_in()
+    app.exec()

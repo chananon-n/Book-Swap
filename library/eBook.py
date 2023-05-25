b50b4aed44a381f377236454437c7217cfe4d94d
chananon-n <91945879+chananon-n@users.noreply.github.com>
2023-05-26 00:47:43 +0700

    Create pdf valuable at ebook module

--- library/eBook.py
+++ library/eBook.py
@@ -5,10 +5,13 @@ from library.abstractBook import abstractBook
 
 
 class eBook(abstractBook):
-    def __init__(self,picture, name, author, description, category,price):
-        super(eBook, self).__init__(picture, name, author, description, category,price)
-        self.pdf = None
+    def __init__(self, picture, name, author, description, category, price):
+        super(eBook, self).__init__(picture, name, author, description, category, price)
+        self.pdf = ""
         self.ID = random.randint(10000, 100000)
+
+    def getPdf(self):
+        return self.pdf
 
     def get_ID(self):
         return self.ID
@@ -22,9 +25,11 @@ class eBook(abstractBook):
     def set_price(self, price):
         self.price = price
 
+    def setPDF(self, pdf):
+        self.pdf = pdf
+
     def get_price(self):
         return self.price
-
 
     def display(self):
         pass

18630949d9ca43915e067f380f3f10fb433f723d
chananon-n <91945879+chananon-n@users.noreply.github.com>
2023-05-26 18:02:52 +0700

    create getAllBook and getAllEbook fix sign_up

--- librarySystem.py
+++ librarySystem.py
@@ -101,6 +101,9 @@ class librarySystem:
                 search_result.append(ebook)
         return search_result
 
+    def createUserID(name):
+        return storageSystem.storageSystem.createNewUser(name)
+
     def filterCategory(self, category):
         bookList = []
         allBook = storageSystem.storageSystem.getAllBooks(self.userID)
@@ -134,6 +137,12 @@ class librarySystem:
 
     def getBookListFromLocal(self):
         return storageSystem.storageSystem.getBooksFromLocal()
+
+    def getAllBooks(self):
+        return self.book_list
+
+    def getAllEbook(self):
+        return self.ebook_list
 
     def checkStatus(self, bookID, userID, status):
         return storageSystem.storageSystem.checkStatusWithLocal(bookID, userID, status)
